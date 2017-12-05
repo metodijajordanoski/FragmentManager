f9ecdb29de15193fd0536bdf3449f3719a2e6d96
Metodija <mendejordanoski@gmail.com>
2017-12-05 18:54:27 +0100

    setUpPager

--- app/src/main/java/prvaaplikacija/a05122017/MainActivity.java
+++ app/src/main/java/prvaaplikacija/a05122017/MainActivity.java
@@ -1,13 +1,29 @@
 package prvaaplikacija.a05122017;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.pager)
+    ViewPager pager;
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setUpViewPager(pager);
+        tabLayout.setupWithViewPager(pager);
+    }
+
+    public void setUpViewPager(ViewPager upViewPager) {
+        MyPageAdapterPom adapter = new MyPageAdapterPom(this.getSupportFragmentManager());
+        pager.setAdapter(adapter);
     }
 }
