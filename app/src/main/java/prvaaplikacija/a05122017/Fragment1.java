69b294886b1a7de5870425a41dedc5aabbf86b3a
Metodija <mendejordanoski@gmail.com>
2017-12-05 19:54:42 +0100

    Butterknife implemented

--- app/src/main/java/prvaaplikacija/a05122017/Fragment1.java
+++ app/src/main/java/prvaaplikacija/a05122017/Fragment1.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by Mende on 05.12.2017.
@@ -13,10 +18,25 @@ import android.view.ViewGroup;
 
 public class Fragment1 extends Fragment {
 
+    private Unbinder mUnbinder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment1,null);
+        mUnbinder = ButterKnife.bind(this, view);
         return view;
+    }
+
+    @OnClick(R.id.button)
+    public void buttonClick(View view) {
+        Toast.makeText(getActivity(),"toast Fragment 1", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mUnbinder.unbind();
     }
 }
---
+++ app/src/main/java/prvaaplikacija/a05122017/Fragment2.java
@@ -0,0 +1,22 @@
+package prvaaplikacija.a05122017;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mende on 05.12.2017.
+ */
+
+public class Fragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment2,null);
+        return view;
+    }
+}
